1ee350b9381b6a11a8644463124e1b9737c72cbb
Angiefiona <angellamumoh@gmail.com>
2023-02-13 16:07:16 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+    var fullName: String= "Fridah Mwaka"
+    println(fullName)
+
+    var age: Byte= 40
+    println(age)
+
+    var phoneNumber: String= "0727835069"
+    println(phoneNumber)
+
+    var weight: Int= 67
+    println(weight)
+
+    var citizenKenya: Boolean= true
+    println(citizenKenya)
+
+}
